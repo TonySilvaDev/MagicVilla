46b1cad2ce1c1bbb46171e3b3889f570e7505404
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-02 11:57:34 -0600

    Crear base de datos con EF Core

---
+++ MagicVilla_API/Datos/ApplicationDbContext.cs
@@ -0,0 +1,15 @@
+﻿using MagicVilla_API.Modelos;
+using Microsoft.EntityFrameworkCore;
+
+namespace MagicVilla_API.Datos
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+
+        }
+
+        public DbSet<Villa> Villas { get; set; }
+    }
+}
