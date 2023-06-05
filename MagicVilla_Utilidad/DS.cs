420e6defec66f2765e671c710129006503b4d6f3
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-05 17:30:57 -0600

    Proyecto Web DTO's y modelos API

---
+++ MagicVilla_Utilidad/DS.cs
@@ -0,0 +1,16 @@
+﻿namespace MagicVilla_Utilidad
+{
+    /// <summary>
+    /// Definiciones estaticas
+    /// </summary>
+    public static class DS
+    {
+        public enum APITipo
+        {
+            GET,
+            POST,
+            PUT,
+            DELETE
+        }
+    }
+}
