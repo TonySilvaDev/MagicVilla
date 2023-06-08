efb9d070bf354b0c98dc3346d75744a97d87c249
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-08 17:39:24 -0600

    API Netural

--- MagicVilla_API/Controllers/UsuarioController.cs
+++ MagicVilla_API/Controllers/UsuarioController.cs
@@ -5,11 +5,11 @@ using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using System.Net;
 
-namespace MagicVilla_API.Controllers.v1
+namespace MagicVilla_API.Controllers
 {
     [Route("api/v{version:apiVersion}/[controller]")]
     [ApiController]
-    [ApiVersion("1.0")]
+    [ApiVersionNeutral]
     public class UsuarioController : ControllerBase
     {
         private readonly IUsuarioRepositorio _usuarioRepo;
