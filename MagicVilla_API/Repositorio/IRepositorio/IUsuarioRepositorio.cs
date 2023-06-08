36e003e7c30443c7944650e2c1840819c542c1d3
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-08 12:05:40 -0600

    Login y registro - modelos y repositorio

---
+++ MagicVilla_API/Repositorio/IRepositorio/IUsuarioRepositorio.cs
@@ -0,0 +1,12 @@
+﻿using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+
+namespace MagicVilla_API.Repositorio.IRepositorio
+{
+    public interface IUsuarioRepositorio
+    {
+        bool IsUsuarioUnico(string userName);
+        Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto);
+        Task<Usuario> Registrar(RegistroRequestDto registroRequestDto);
+    }
+}
---
+++ MagicVilla_API/Repositorio/UsuarioRepositorio.cs
@@ -0,0 +1,92 @@
+﻿using MagicVilla_API.Datos;
+using MagicVilla_API.Modelos;
+using MagicVilla_API.Modelos.Dto;
+using MagicVilla_API.Repositorio.IRepositorio;
+using Microsoft.IdentityModel.Tokens;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace MagicVilla_API.Repositorio
+{
+    public class UsuarioRepositorio : IUsuarioRepositorio
+    {
+        private readonly ApplicationDbContext _db;
+        private string secretKey;
+
+        public UsuarioRepositorio(ApplicationDbContext db, IConfiguration configuration)
+        {
+            _db = db;
+            secretKey = configuration.GetValue<string>("ApiSettings:Secret");
+        }
+
+        public bool IsUsuarioUnico(string userName)
+        {
+            var usuario = _db.Usuarios.FirstOrDefault(u => u.UserName.ToLower() == userName.ToLower());
+
+            if (usuario == null)
+            {
+                return true;
+            }
+
+            return false;
+        }
+
+        public async Task<LoginResponseDto> Login(LoginRequestDto loginRequestDto)
+        {
+            var usuario = _db.Usuarios.FirstOrDefault(u => u.UserName.ToLower() == loginRequestDto.UserName.ToLower() &&
+                                                           u.Password == loginRequestDto.Password);
+
+            if (usuario == null)
+            {
+                return new LoginResponseDto()
+                {
+                    Token = "",
+                    Usuario = null
+                };
+            }
+
+            //Si usuaio existe, generamos el JW Token
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes(secretKey);
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(new Claim[]
+                {
+                    new Claim(ClaimTypes.Name, usuario.Id.ToString()),
+                    new Claim(ClaimTypes.Role, usuario.Rol)
+                }),
+                Expires = DateTime.UtcNow.AddDays(7),
+                SigningCredentials = new(new SymmetricSecurityKey(key), SecurityAlgorithms.HmacSha256Signature)
+            };
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            LoginResponseDto loginResponseDto = new()
+            {
+                Token = tokenHandler.WriteToken(token),
+                Usuario = usuario
+            };
+
+            return loginResponseDto;
+        }
+
+        public async Task<Usuario> Registrar(RegistroRequestDto registroRequestDto)
+        {
+            Usuario usuario = new()
+            {
+                UserName = registroRequestDto.UserName,
+                Password = registroRequestDto.Password,
+                Nombres = registroRequestDto.Nombres,
+                Rol = registroRequestDto.Rol
+            };
+
+            await _db.Usuarios.AddAsync(usuario);
+            await _db.SaveChangesAsync();
+
+            // Quitar password para no regresarlo
+            usuario.Password = "";
+
+            return usuario;
+        }
+    }
+}
