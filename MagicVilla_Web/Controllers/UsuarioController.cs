ebee0a923455ed06ba6f99cc5e276e3c117f1dc0
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-08 15:42:43 -0600

    Usuario controller - metodos de accion

---
+++ MagicVilla_Web/Controllers/UsuarioController.cs
@@ -0,0 +1,57 @@
+﻿using MagicVilla_Web.Models;
+using MagicVilla_Web.Models.Dto;
+using MagicVilla_Web.Services.IServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MagicVilla_Web.Controllers
+{
+    public class UsuarioController : Controller
+    {
+        private readonly IUsuarioService _usuarioService;
+
+        public UsuarioController(IUsuarioService usuarioService)
+        {
+            _usuarioService = usuarioService;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Index(LoginRequestDto dto)
+        {
+            return View();
+        }
+
+        public IActionResult Registrar()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Registrar(RegistroRequestDto modelo)
+        {
+            var response = await _usuarioService.Registrar<APIResponse>(modelo);
+
+            if (response != null && response.IsExitoso)
+            {
+                return RedirectToAction("login");
+            }
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            return View();
+        }
+
+        public IActionResult AccesoDenegado()
+        {
+            return View();
+        }
+    }
+}
