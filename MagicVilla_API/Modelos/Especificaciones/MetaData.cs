f9a142e0af5263dd33f73d12ea274c0f086f55b0
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-08 22:22:06 -0600

    Pagination en API - Server Side Pagination

---
+++ MagicVilla_API/Modelos/Especificaciones/MetaData.cs
@@ -0,0 +1,9 @@
+﻿namespace MagicVilla_API.Modelos.Especificaciones
+{
+    public class MetaData
+    {
+        public int TotalPages { get; set; } // Total de paginas
+        public int PageSize { get; set; } // Registros por pagina
+        public int TotalCount { get; set; } // Total de registros
+    }
+}
