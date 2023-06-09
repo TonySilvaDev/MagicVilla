bdae29e1ac62de406867a413407e94a8369674da
Antonio Silva <antonio.silvamnrl@gmail.com>
2023-06-08 22:51:59 -0600

    Pagination en Web

--- MagicVilla_Web/Models/APIRequest.cs
+++ MagicVilla_Web/Models/APIRequest.cs
@@ -7,7 +7,13 @@ namespace MagicVilla_Web.Models
         public APITipo APITipo { get; set; } = APITipo.GET;
         public string Url { get; set; }
         public object Datos { get; set; }
-
         public string Token { get; set; }
+        public Parametros Parametros { get; set; }
+    }
+
+    public class Parametros
+    {
+        public int PageNumber { get; set; }
+        public int PageSize { get; set; }
     }
 }
